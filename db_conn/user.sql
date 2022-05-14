905a4e168c867bbef6e0d6cffd1eab844de8aca2
JamesMok0916 <86011275+James000916@users.noreply.github.com>
2022-05-14 22:37:54 +0800

    1

--- db_conn/user.sql
+++
@@ -1,7 +0,0 @@
-create table users (
-	user_id int(11) AUTO_INCREMENT,
-	user_email varchar(50) NOT NULL,
-	user_name varchar(50) NOT NULL,
-	user_pwd varchar(255) NOT NULL,
-	PRIMARY KEY (user_id)
-);
